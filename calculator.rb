1e23f8fdcb2258301e10a517a453ac485af5852e
Andrew Johnston <johnto20@hotmail.com>
2015-10-21 19:08:57 +1100

    calculator update

--- calculator.rb
+++ calculator.rb
@@ -12,24 +12,17 @@ num2 = gets.chomp
 operator = 0
 
 loop do
-say  "What do you want to do to the numbers?: 1) ADD, 2) SUBTRACT, 3)DIVIDE, 4)MULTIPLY"
-operator = gets.chomp
-if operator.to_i < 5
-  break
-end
+  say  "What do you want to do to the numbers?: 1) ADD, 2) SUBTRACT, 3)DIVIDE, 4)MULTIPLY"
+  operator = gets.chomp
+  break if %w(1 2 3 4).include? operator
 end
 
-if operator == '1'
-  answer = num1.to_i + num2.to_i
-  say "Your answer is #{answer}"
-elsif operator == '2'
-  answer = num1.to_i - num2.to_i
-  say "Your answer is #{answer}"
-elsif operator == '3'
-  answer = num1.to_f / num2.to_f
-  say "Your answer is #{answer}"
-  else
-  answer = num1.to_i * num2.to_i
-  say "Your answer is #{answer}"
-end 
+answer = case operator
+  when '1' then num1.to_i + num2.to_i
+  when '2' then num1.to_i - num2.to_i
+  when '3' then num1.to_f / num2.to_f
+  when '4' then num1.to_i * num2.to_i
+  
+end
 
+say "Your answer is #{answer}"
